bcf38214cc28b897a415b8120c5ebf0c0028fe49
Tommy109 <55705991+Tommy109@users.noreply.github.com>
2023-01-29 19:52:39 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from "vue";
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
+
+const p = ref(person);
+
+function addToCount() {
+  p.value.age = p.value.age + 1;
+}
+
+function subtractFromCount() {
+  p.value.age = p.value.age - 1;
+}
+</script>
+
+<template>
+  <main>
+    <div>
+      <h2>Name: {{p.firstName}}</h2>
+      <h2>Last: {{p.lastName}}</h2>
+      <h1>{{ p.age }}</h1>
+      <button @click="subtractFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  width: 100vw;
+  height: 100vm;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  margin-top: 10%;
+  text-align: center;
+}
+</style>
